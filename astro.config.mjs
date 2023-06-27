e218ba253c534bbcd54a3d096214b3e6bccb3a5b
KareeMagdyy <karim67899@gmail.com>
2023-06-27 23:43:31 +0300

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,32 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import astroI18next from "astro-i18next";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://kareem-magdy.com/",
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          ar: "ar",
+        },
+      },
+    }),
+    robotsTxt({
+      sitemap: "https://kareem-magdy.com/sitemap-index.xml",
+      host: "kareem-magdy.com",
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    astroI18next(),
+  ],
+});
---
+++ public/assets/js/main.js
@@ -0,0 +1,126 @@
+// <-- //stats number counter -->
+$(".counter").countUp();
+// <-- disable body scroll which navbar is in active -->
+$(function () {
+  $(".navbar-toggler").click(function () {
+    $("body").toggleClass("noscroll");
+  });
+});
+// <--/MENU-JS-->
+$(window).on("scroll", function () {
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 80) {
+    $("#site-header").addClass("nav-fixed");
+  } else {
+    $("#site-header").removeClass("nav-fixed");
+  }
+});
+//Main navigation Active Class Add Remove
+$(".navbar-toggler").on("click", function () {
+  $("header").toggleClass("active");
+});
+$(document).on("ready", function () {
+  if ($(window).width() > 991) {
+    $("header").removeClass("active");
+  }
+  $(window).on("resize", function () {
+    if ($(window).width() > 991) {
+      $("header").removeClass("active");
+    }
+  });
+});
+
+//Typing Text
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Freelancer", "Web developer"];
+const typingDelay = 250;
+const erasingDelay = 20;
+const newTextDelay = 150; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    // add class 'typing' if there's none
+    if (!cursorSpan.classList.contains("typing")) {
+      cursorSpan.classList.add("typing");
+    }
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, 0);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+//Testimonials
+$(document).ready(function () {
+  $("#owl-demo1").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      736: {
+        items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 2,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+});
+
+// Move To top
+const topBtn = document.querySelector("#movetop");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.cssText = `transform: translateX(0)`;
+  } else {
+    topBtn.style.cssText = `transform: translateX(100px)`;
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+topBtn.addEventListener("click", topFunction);
