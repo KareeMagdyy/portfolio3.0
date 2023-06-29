cf47b3a318adbaaf3b3d754f1ec7b87db927fde5
KareeMagdyy <karim67899@gmail.com>
2023-06-29 14:28:02 +0300

    adding logic for forms

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -41,42 +41,42 @@ const erasingDelay = 20;
 const newTextDelay = 150; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
-
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if (!cursorSpan.classList.contains("typing"))
-      cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(erase, newTextDelay);
-  }
-}
-
-function erase() {
-  if (charIndex > 0) {
-    // add class 'typing' if there's none
-    if (!cursorSpan.classList.contains("typing")) {
-      cursorSpan.classList.add("typing");
+if (typedTextSpan) {
+  function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+      if (!cursorSpan.classList.contains("typing"))
+        cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+    } else {
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
     }
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, 0);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-    setTimeout(type, typingDelay);
   }
+
+  function erase() {
+    if (charIndex > 0) {
+      // add class 'typing' if there's none
+      if (!cursorSpan.classList.contains("typing")) {
+        cursorSpan.classList.add("typing");
+      }
+      typedTextSpan.textContent = textArray[textArrayIndex].substring(0, 0);
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+    } else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+      setTimeout(type, typingDelay);
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // On DOM Load initiate the effect
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+  });
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  // On DOM Load initiate the effect
-  if (textArray.length) setTimeout(type, newTextDelay + 250);
-});
-
 //Testimonials
 $(document).ready(function () {
   $("#owl-demo1").owlCarousel({
--- public/assets/js/theme-change.js
+++ public/assets/js/theme-change.js
@@ -3,22 +3,37 @@ const toggleSwitch = document.querySelector(
 );
 const currentTheme = localStorage.getItem("theme");
 
+const mainLogo = document.querySelector(".navbar-brand .main-logo");
+const darkThemeLogo = "/assets/images/logo-light.png";
+const lightThemeLogo = "/assets/images/logo-black.png";
+
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
+  mainLogo.src = lightThemeLogo;
 
-  if (currentTheme === "dark") {
+  if (currentTheme === "light") {
     toggleSwitch.checked = true;
+    mainLogo.src = lightThemeLogo;
   }
 }
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
     document.documentElement.setAttribute("data-theme", "light");
+    mainLogo.src = lightThemeLogo;
     localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    mainLogo.src = darkThemeLogo;
+    localStorage.setItem("theme", "dark");
   }
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
+window.addEventListener("load", () => {
+  if (currentTheme === null || currentTheme === "dark") {
+    mainLogo.src = darkThemeLogo;
+  } else {
+    mainLogo.src = lightThemeLogo;
+  }
+});
