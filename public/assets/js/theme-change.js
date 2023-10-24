05f972468f286194181697706152ff8df57bc35a
Kareem El-Zomor <karim67899@gmail.com>
2023-10-24 13:10:54 +0300

    adding Palastine Logo on navbar

--- public/assets/js/theme-change.js
+++ public/assets/js/theme-change.js
@@ -4,6 +4,7 @@ const toggleSwitch = document.querySelector(
 const currentTheme = localStorage.getItem("theme");
 
 const mainLogo = document.querySelector(".navbar-brand .main-logo");
+const palastine = document.querySelector("#palastine");
 const darkThemeLogo =
   "https://res.cloudinary.com/dadonc0ng/image/upload/v1689584903/Portfolio/Main%20Assets/logo-light_izhwdz.webp";
 const lightThemeLogo =
@@ -12,10 +13,12 @@ const lightThemeLogo =
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
   mainLogo.src = lightThemeLogo;
+  palastine.src = "/assets/images/p-dark2.jpg";
 
   if (currentTheme === "light") {
     toggleSwitch.checked = true;
     mainLogo.src = lightThemeLogo;
+    palastine.src = "/assets/images/p-dark2.jpg";
   }
 }
 
@@ -23,10 +26,12 @@ function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "light");
     mainLogo.src = lightThemeLogo;
+    palastine.src = "/assets/images/p-light2.jpg";
     localStorage.setItem("theme", "light");
   } else {
     document.documentElement.setAttribute("data-theme", "dark");
     mainLogo.src = darkThemeLogo;
+    palastine.src = "/assets/images/p-dark2.jpg";
     localStorage.setItem("theme", "dark");
   }
 }
